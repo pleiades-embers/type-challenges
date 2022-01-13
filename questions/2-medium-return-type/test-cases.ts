15b81907aeffc7f729eb6a5c9efdad72c2f4987e
embers <935267851@qq.com>
2022-01-13 10:26:29 +0800

    update code

--- questions/2-medium-return-type/test-cases.ts
+++ questions/2-medium-return-type/test-cases.ts
@@ -1,5 +1,14 @@
 import { Equal, Expect } from '@type-challenges/utils'
 
+type ComplexObject = {
+  a: [12, 'foo']
+  bar: 'hello'
+  prev(): number
+}
+
+const fn = (v: boolean) => v ? 1 : 2
+const fn1 = (v: boolean, w: any) => v ? 1 : 2
+
 type cases = [
   Expect<Equal<string, MyReturnType<() => string>>>,
   Expect<Equal<123, MyReturnType<() => 123>>>,
@@ -9,12 +18,3 @@ type cases = [
   Expect<Equal<1 | 2, MyReturnType<typeof fn>>>,
   Expect<Equal<1 | 2, MyReturnType<typeof fn1>>>,
 ]
-
-type ComplexObject = {
-  a: [12, 'foo']
-  bar: 'hello'
-  prev(): number
-}
-
-const fn = (v: boolean) => v ? 1 : 2
-const fn1 = (v: boolean, w: any) => v ? 1 : 2
